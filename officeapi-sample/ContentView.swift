826cbdbdaf59a0ab1ac6e9266bf0c3d055285b24
Shariq Hirani <shariqhirani2010@gmail.com>
2020-02-27 10:30:56 -0600

    Initial Commit

---
+++ officeapi-sample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  officeapi-sample
+//
+//  Created by Shariq Hirani on 2/27/20.
+//  Copyright © 2020 Shariq Hirani. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
